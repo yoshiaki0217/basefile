c30bc7866c8aedfdc74d62c4f23e34cab40d5d90
大城由晃 <yoshiaki19980217@icloud.com>
2021-04-02 16:10:03 +0900

    設定

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 ln -fns ~/dotfiles/Brewfile ~/Brewfile
-ln -fns ~/dotfiles/Brewfile.lock.json ~/Brewfile.lock.json
+# ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
+# ln -fns ~/dotfiles/.config/fish ~/.config/fish
+# ln -fns ~/dotfiles/.tmux.conf ~/.tmux.conf
